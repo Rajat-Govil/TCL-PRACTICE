3cf49b1f69516123ce79ac4358f118a2ce260cff
Rajat Govil <91873360+Rajat-Govil@users.noreply.github.com>
2024-07-31 22:26:13 +0530

    Update PROBLEM_25.tcl

--- PROBLEM_25.tcl
+++ PROBLEM_25.tcl
@@ -6,12 +6,9 @@ puts "IP Address :\t$ip"
 set iplist [split $ip "."]
 set newip {}
 
-foreach i {0 3} {
+foreach i {0 2 1 3} {
 	lappend newip [lindex $iplist $i]
 }
-
-set newip [linsert $newip 1 [lindex $iplist 2]]
-set newip [linsert $newip 2 [lindex $iplist 1]]		
 
 set ip [join $newip "."]
 puts "New ip address:\t$ip"
